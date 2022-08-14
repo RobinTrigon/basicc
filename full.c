4b2f1af2a6387a77ac9150121fab95105cd4dfc7
Robin Trigon <86462148+RobinTrigon@users.noreply.github.com>
2022-08-14 22:41:36 +0600

    Update full.c

--- full.c
+++ full.c
@@ -63,19 +63,75 @@ printf("-----------calculator------------\n");
 float numb1, numb2, numb3, result;
 printf("please enter two number(4 4): ");
 scanf("%f %f",&numb1, &numb2);
-
 result = numb1 + numb2;
 printf("%.2f + %.2f = %.2f \n", numb1, numb2, result);
 result = numb1 - numb2;
-
 printf("%.2f - %.2f = %.2f \n", numb1, numb2, result);
 result = numb1 * numb2;
-
 printf("%.2f * %.2f = %.2f \n", numb1, numb2, result);
 result = numb1 / numb2;
-
 printf("%.2f / %.2f = %.2f \n", numb1, numb2, result);
 
+// signed Integer data 
+printf("-----------signed Integer data------------\n");
+// Short -> %d , %hi
+short int pk = 45; 
+printf("I'm short %hi\n", pk);
+// Int -> %d %i
+signed int ps = 100000;
+printf("I'm int %i\n",ps);
+// long -> %ld ,%li
+long int pl = 1337l;
+printf("I'm long %ld\n", pl);
+// Long Long %lld, %lli
+long long int pll =  13337LL;
+printf("I'm long long %lld\n", pll);
+// Octal int -> %o
+int oc = 077;// need to use 0 at 1st.
+printf("I'm octal %o\n",oc);
+printf("decimal of %o is %d\n",oc, oc);// convert octal to decimal
+// hex int -> %x .%X
+int hx = 0xfff;// need to use 0x at 1st.
+printf("I'm hex %x\n",hx);
+printf(" decimal of %x is %d\n",hx, hx);// convert hex to decimal
+
+// unsigned Integer data 
+printf("-----------unsigned Integer data------------\n");
+//unsinged means all positive data
+
+//unsigned short -> %hu
+unsigned short us = 41;
+printf(" I'm Unsigned short %hu\n",us);
+
+//unsigned int -> %u
+unsigned int ui = 45;
+printf(" I'm Unsigned int %u\n",ui);
+
+//unsigned long -> %lu
+unsigned long ul = 55;
+printf(" I'm Unsigned long %lu\n",ul);
+
+//unsigned long long -> %llu
+unsigned long long ull = 133337;
+printf(" I'm Unsigned long long %llu\n",ull);
+
+// if we stored negative number into unsinged
+unsigned short negative = -2;
+printf(" I'm Unsigned negative %hu\n",negative); // it give us a unexpected result coz , binary of -2 is 1111111111111110 and decimal of this binary value is 65534. so we can see '65534' as a result.
+
+
+// floating data 
+printf("-----------floating data------------\n");
+
+// float -> %f
+float fd = 489.123F;
+printf(" I'm float %f\n",fd);
+// Double -> %lf
+double df = 1337.33;
+printf(" I'm double float %lf\n",df);
+// Long Double -> %Lf
+long double lf = 133333337.33; 
+printf(" I'm long double float %Lf\n",lf);// you can use %.2Lf for print 133333337.33
 
 return 0;
 }
